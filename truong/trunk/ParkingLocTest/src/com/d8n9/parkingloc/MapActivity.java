cd21ad8ade346513fde55b86f78cdaadc6aadb86
thanhtruong.np@gmail.com <thanhtruong.np@gmail.com@e224cd26-16d4-85f3-3897-192385dd4cfe>
2013-04-25 14:20:43 +0000

    Add working Google Maps

--- truong/trunk/ParkingLocTest/src/com/d8n9/parkingloc/MapActivity.java
+++ truong/trunk/ParkingLocTest/src/com/d8n9/parkingloc/MapActivity.java
@@ -1,11 +1,40 @@
 package com.d8n9.parkingloc;
 
-import android.app.Activity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import android.app.AlertDialog;
 import android.os.Bundle;
- 
-public class MapActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map);
-    }
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MapActivity extends FragmentActivity {
+	
+	final int RQS_GooglePlayServices = 1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+		
+		if (resultCode == ConnectionResult.SUCCESS){
+			Toast.makeText(getApplicationContext(), 
+					"isGooglePlayServicesAvailable SUCCESS", 
+					Toast.LENGTH_LONG).show();
+		}else{
+			GooglePlayServicesUtil.getErrorDialog(resultCode, this, RQS_GooglePlayServices);
+		}
+		
+	}
+
 }
