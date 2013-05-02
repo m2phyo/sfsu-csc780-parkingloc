b63eb90e2a9712a1ab3864e2bc6747226e50fa54
thanhtruong.np@gmail.com <thanhtruong.np@gmail.com@e224cd26-16d4-85f3-3897-192385dd4cfe>
2013-05-02 11:47:22 +0000

    - Smooth zoom - Drag-able marker - Home location button - Current location button

--- truong/trunk/ParkingLocTest/src/com/d8n9/parkingloc/MapActivity.java
+++ truong/trunk/ParkingLocTest/src/com/d8n9/parkingloc/MapActivity.java
@@ -1,6 +1,7 @@
 package com.d8n9.parkingloc;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -9,14 +10,17 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.LocationSource.OnLocationChangedListener;
 import com.google.android.gms.maps.MapFragment;
 //import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.CancelableCallback;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -38,7 +42,8 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
  
-public class MapActivity extends Activity implements OnMapClickListener, OnMapLongClickListener, LocationSource, LocationListener{
+public class MapActivity extends Activity 
+			implements OnMapClickListener, OnMapLongClickListener, LocationSource, LocationListener, OnMarkerDragListener{
 	
 	final int RQS_GooglePlayServices = 1;
 	private GoogleMap myMap;
@@ -46,12 +51,18 @@ public class MapActivity extends Activity implements OnMapClickListener, OnMapLo
 	Location myLocation;
 	TextView tvLocInfo;
 	
+	ArrayList<LatLng> listPoint = new ArrayList<LatLng>();
+	boolean homeMarkerAdded = false;
+	boolean currentLocationMarkerAdded = false;
+	
 	LocationManager myLocationManager = null;
 	OnLocationChangedListener myLocationListener = null;
 	Criteria myCriteria;
 	
 	static final LatLng USA = new LatLng(37.090240, -95.712891);
-
+	static final LatLng currentLocation = new LatLng(37.723886, -122.477067);
+	static final LatLng home = new LatLng(37.782426, -122.416223);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -65,7 +76,8 @@ public class MapActivity extends Activity implements OnMapClickListener, OnMapLo
 		myMap = myMapFragment.getMap();
 		
 		myMap.setMyLocationEnabled(true);
-		myMap.getUiSettings().setZoomControlsEnabled(false);
+		myMap.getUiSettings().setMyLocationButtonEnabled(false);
+		myMap.getUiSettings().setZoomControlsEnabled(true);
 		
 		myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 		//myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -76,17 +88,39 @@ public class MapActivity extends Activity implements OnMapClickListener, OnMapLo
 		myCriteria.setAccuracy(Criteria.ACCURACY_FINE);
 		myLocationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 		
-		// Move the camera to USA
-		myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(USA, 3));	
-		// Zoom in, animating the camera.
-	    // myMap.animateCamera(CameraUpdateFactory.zoomTo(3), 2000, null);
-		
+		// Move the camera to USA and zoom to 3 within 2500 ms
+		myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(USA, 3), 2500, null);	
+
 		// Detect single tap on the map
 		myMap.setOnMapClickListener(this);
 		
 		// Detect long tap on the map
 		myMap.setOnMapLongClickListener(this);
 		
+		// 
+		myMap.setOnMarkerDragListener(this);
+		
+		// Home button on click
+		findViewById(R.id.home_button).setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+            	if (!homeMarkerAdded) {
+            		myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(home, 14), 3000, addHomeMarkerCallback);
+            	} else {
+            		myMap.animateCamera(CameraUpdateFactory.newLatLng(home), 1500, null);
+            	}
+            }
+        });
+		
+		// Current location button on click
+		findViewById(R.id.current_location).setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+            	if (!currentLocationMarkerAdded) {
+            		myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14), 3000, addCurrentLocationMarkerCallback);
+            	} else {
+            		myMap.animateCamera(CameraUpdateFactory.newLatLng(currentLocation), 1500, null);
+            	}
+            }
+        });
 //		
 //		Geocoder g = new Geocoder(this, Locale.getDefault());
 //		try {
@@ -107,30 +141,30 @@ public class MapActivity extends Activity implements OnMapClickListener, OnMapLo
 		
 		
 		// Geocoder: translate an address to longitude and latitude
-		Geocoder g = new Geocoder(this);
-	    List<Address> addressList = null;
-	    String streetName = "San Francisco State University";
-	    try {
-	        addressList = g.getFromLocationName(streetName, 1);
-
-	    } catch (IOException e) {
-	        Toast.makeText(this, "Location not found",     Toast.LENGTH_SHORT)
-	                    .show();
-	            e.printStackTrace();
-
-	    } finally {
-	    	if (addressList.get(0) != null) {
-		        Address address = addressList.get(0);
-	
-		        if (address.hasLatitude() && address.hasLongitude()) {
-		            double selectedLat = address.getLatitude();
-		            double selectedLng = address.getLongitude();
-		            LatLng place = new LatLng(selectedLat, selectedLng);
-		            myMap.addMarker(new MarkerOptions().position(place).title("Here is the road location")
-		            			.snippet("Hon the lads"));
-		        }
-	    	}
-	    }
+//		Geocoder g = new Geocoder(this);
+//	    List<Address> addressList = new ArrayList<Address>();
+//	    String streetName = "San Francisco State University";
+//	    try {
+//	        addressList = g.getFromLocationName(streetName, 1);
+//
+//	    } catch (IOException e) {
+//	        Toast.makeText(this, "Location not found",     Toast.LENGTH_SHORT)
+//	                    .show();
+//	            e.printStackTrace();
+//
+//	    } finally {
+//	    	if (addressList.get(0) != null) {
+//		        Address address = addressList.get(0);
+//	
+//		        if (address.hasLatitude() && address.hasLongitude()) {
+//		            double selectedLat = address.getLatitude();
+//		            double selectedLng = address.getLongitude();
+//		            LatLng place = new LatLng(selectedLat, selectedLng);
+//		            myMap.addMarker(new MarkerOptions().position(place).title("Here is the road location")
+//		            			.snippet("Hon the lads"));
+//		        }
+//	    	}
+//	    }
 	}
 
 	@Override
@@ -160,18 +194,6 @@ public class MapActivity extends Activity implements OnMapClickListener, OnMapLo
 			GooglePlayServicesUtil.getErrorDialog(resultCode, this, RQS_GooglePlayServices);
 		}
 		
-	}
-
-	@Override
-	public void onMapClick(LatLng point) {
-		tvLocInfo.setText(point.toString());
-		myMap.animateCamera(CameraUpdateFactory.newLatLng(point));
-	}
-	
-	@Override
-	public void onMapLongClick(LatLng point) {
-		tvLocInfo.setText("New marker added@" + point.toString());
-		myMap.addMarker(new MarkerOptions().position(point).title(point.toString()));
 	}
 
 	@Override
@@ -223,6 +245,69 @@ public class MapActivity extends Activity implements OnMapClickListener, OnMapLo
 
 	@Override
 	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	CancelableCallback addHomeMarkerCallback =
+		new CancelableCallback(){
+			@Override
+			public void onCancel() {
+			}
+			
+			@Override
+			public void onFinish() {
+				if (!homeMarkerAdded) {
+            		myMap.addMarker(new MarkerOptions().position(home).title("Home").icon(BitmapDescriptorFactory
+            	            .fromResource(R.drawable.home2)));
+            		homeMarkerAdded = true;
+            	}
+			}
+		};
+		
+	CancelableCallback addCurrentLocationMarkerCallback =
+			new CancelableCallback(){
+				@Override
+				public void onCancel() {
+				}
+				
+				@Override
+				public void onFinish() {
+					if (!currentLocationMarkerAdded) {
+	            		myMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location").icon(BitmapDescriptorFactory
+	            	            .fromResource(R.drawable.current_location)));
+	            		currentLocationMarkerAdded = true;
+	            	}
+				}
+			};
+
+	@Override
+	public void onMapClick(LatLng point) {
+		tvLocInfo.setText(point.toString());
+		myMap.animateCamera(CameraUpdateFactory.newLatLng(point));
+	}
+	
+	@Override
+	public void onMapLongClick(LatLng point) {
+		tvLocInfo.setText("New marker added @ " + point.toString());
+		myMap.addMarker(new MarkerOptions().position(point).title(point.toString()).draggable(true));
+		listPoint.add(point);
+	}
+
+	@Override
+	public void onMarkerDrag(Marker marker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMarkerDragEnd(Marker marker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMarkerDragStart(Marker marker) {
 		// TODO Auto-generated method stub
 		
 	}
