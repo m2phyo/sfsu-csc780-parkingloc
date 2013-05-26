5812bae3c95a614427458c80f2310afd4aa75a83
minminphyo@gmail.com <minminphyo@gmail.com@e224cd26-16d4-85f3-3897-192385dd4cfe>
2013-05-26 08:27:36 +0000

    End of Semester, Project final clean up and comments added.

--- min/ParkingLocPHP/getLocation.php
+++ min/ParkingLocPHP/getLocation.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * Get Location Information 
+ * Get Location Information : perform varieties of tasks requested by the 
+ * user on mobile device and response related information and data.
  * 
  * @author Min
+ * @param string $task Type of tasks to perform.
+ * @return Json Encode Object Location Information depend on user request.
  */
 define("DB_HOST", "sfsuswe.com");
 define("DB_USER", "mphyo");
@@ -10,18 +13,9 @@ define("DB_PASSWORD", "910622211");
 define("DB_DATABASE", "student_mphyo");
 // Array for JSON response
 $response = array();
-
-//Checking for post data
-//if (isset($_POST['tag'])) {
-    //getting task
-     //$task = 'update';
-    //$lat = 37.783133; $long = -122.4185093; $take = 1; $id = 2;
-//    echo $latitude = doubleval($lat);
-//    echo $longitude = doubleval($long);
-    //echo $isTaken = intval($take);
-//    echo $address = "";
-//    echo $description = "";
-    
+    //Declaring Post Data variables
+    $take = 0;
+    $insertIsTaken = intval($take);
     $task = $_POST['tag'];
     $id = $_POST['loc_id'];
     $name = $_POST['loc_name'];
@@ -34,18 +28,23 @@ $response = array();
     // DB connection
     $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     
- // check for tag type
+ 
  /**
-  * Get Location Info data set from table.
+  * Get Location Info data set from table for available parking spots which also
+  * is not Home location.
+  * 
+  * @param String $isTaken the info to check the location is available or taken
+  * @param type $name Description
  */
+ // check for task
  if ($task == 'get') {
     // get locations from location_info table
-    $result = mysqli_query($db, "SELECT * FROM location_info WHERE loc_isTaken = '$isTaken'");
+    $result = mysqli_query($db, "SELECT * FROM location_info WHERE loc_isTaken = '$isTaken' AND loc_name != 'Home'");
 
     // check for empty result
     if (mysqli_num_rows($result) > 0) {
         // looping through all results
-        // location id
+        // locations info
         $response["location_info"] = array();
  
         while ($row = mysqli_fetch_array($result)) {
@@ -71,7 +70,7 @@ $response = array();
         echo json_encode($response);
     } 
     else {
-        // no user found
+        // no query found
         $response["success"] = 0;
         $response["message"] = "Retrieving Data Failed!!!";
  
@@ -80,15 +79,22 @@ $response = array();
     }
  }
  
-  // check for tag type
+
  /**
-  * Insert single data set into table.
- */
+  * Insert single data set of available parking spot into table.
+  * 
+  * @param String $name Location Name
+  * @param String $latitude Location Latitude
+  * @param String $longitude Location Longitude
+  * @param  Integer $insertIsTaken Location available information
+  * @return JSON Encoded Object Response message for newly added location.
+ */  
+// check for task
  if ($task == 'insert') {
-    // get locations from location_info table
-    $result = mysqli_query($db, "INSERT INTO location_info (loc_latitude, 
-        loc_longitude, loc_isTaken, loc_address, loc_description) 
-        VALUES('$latitude', '$longitude', '$isTaken', '$address', '$description')");
+    // insert locations into location_info table
+    $result = mysqli_query($db, "INSERT INTO location_info (loc_name, loc_latitude, 
+        loc_longitude, loc_isTaken) 
+        VALUES('$name', '$latitude', '$longitude', '$insertIsTaken')");
 
     // check for empty result
     if ($result) {
@@ -101,53 +107,74 @@ $response = array();
         echo json_encode($response);
     } 
     else {
-        // no user found
+        // not success
         $response["success"] = 0;
         $response["message"] = "Insert Location Data Failed!!!";
  
-        // echo no users JSON
+        // echo JSON
         echo json_encode($response);
     }
  }
  
- // check for tag type
+ 
  /**
-  * Check the location is taken or not.
+  * Update the parking spot location is taken or available.
+  * 
+  * @param String $user_id User ID
+  * @param String $loc_id Location ID
+  * @return JSON Object Related information and message.
  */
+ // check for tag type
  if ($task == 'check') {
-    // get locations from location_info table
+     $user_id = $_POST['user_id'];
+    // get parking spot information from location_info table
     $result = mysqli_query($db, "SELECT * FROM location_info 
-        WHERE loc_latitude = '$latitude' AND loc_longitude = '$longitude'");
+        WHERE loc_id = '$id' AND loc_isTaken = '0'");
 
     // check for empty result
     if (mysqli_num_rows($result) > 0) {
+        //update location isTaken
+        $updateResult = mysqli_query($db, "UPDATE location_info SET loc_isTaken='1'
+        WHERE loc_id='$id'");
+        //update user's reserve parking spot information.
+        $updateUserReserve = mysqli_query($db, "UPDATE users SET reserved_id = '$id'
+                WHERE user_id = '$user_id'");
+        
+        if($updateResult && $updateUserReserve){
+            // success
+            $response["success"] = 1;
+            $response["message"] = "Reserve Successful!!!";
  
-        while ($row = mysqli_fetch_array($result)) {
-            // get isTaken data
-            $response["loc_isTaken"] = $row["loc_isTaken"];
- 
-        }       
-        // success
-        $response["success"] = 1;
-        $response["message"] = "Retrieving isTaken Data Successful!!!";
- 
-        // echoing JSON response
-        echo json_encode($response);
+            // echoing JSON response
+            echo json_encode($response);
+        }
+        else {
+            // not success
+            $response["success"] = 0;
+            $response["message"] = "Update Error!!!";
+
+            // echo no users JSON
+            echo json_encode($response);
+        }
     } 
     else {
-        // no user found
+        // if the spot is taken.
         $response["success"] = 0;
-        $response["message"] = "Retrieving isTaken Data Failed!!!";
+        $response["message"] = "Sorry The Spot is Taken!!!";
  
         // echo no users JSON
         echo json_encode($response);
     }
  }
- 
- // check for tag type
+  
  /**
   * Update the location is taken or not taken.
+  * 
+  * @param String $user_id User ID
+  * @param String $loc_id Location ID
+  * @return JSON Object Related information and message.
  */
+// check for tag type
  if ($task == 'update') {
     // update the location isTaken
     $result = mysqli_query($db, "UPDATE location_info SET loc_isTaken='$isTaken'
@@ -172,6 +199,112 @@ $response = array();
         echo json_encode($response);
     }
  }
+  
+ /**
+  * Release the reserved location back to available.
+  * 
+  * @param String $user_id User ID to look for related user's reserved location ID
+  * @return JSON Object Related information and message.
+ */
+// check for tag type
+ if ($task == 'release') {
+     $user_id = $_POST['user_id'];
+     
+     //Check user has reserved location.
+    $checkIsTaken = mysqli_query($db, "SELECT reserved_id FROM users 
+        WHERE user_id = '$user_id'");
+    $row = mysqli_fetch_array($checkIsTaken);
+    
+    if(is_null($row["reserved_id"])){
+        // no reserved location
+        $response["success"] = 0;
+        $response["message"] = "No Reserved Spot to Release!!!";
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }else{
+        // reserved location
+        $reserve_loc_id =  $row["reserved_id"];
+        // update the location isTaken back to '0' which means available.
+        $result = mysqli_query($db, "UPDATE location_info SET loc_isTaken = '0'
+            WHERE loc_id = '$reserve_loc_id'");
+        //update location info to null which means user has no reserved spot.
+        $resultUser = mysqli_query($db, "UPDATE users SET reserved_id = NULL
+            WHERE user_id='$user_id'");
+
+        // check for empty result
+        if ($result && $resultUser) {
+
+            // success
+            $response["success"] = 1;
+            $response["message"] = "Updating Release Successful!!!";
+
+            // echoing JSON response
+            echo json_encode($response);
+        } 
+        else {
+            // not success
+            $response["success"] = 0;
+            $response["message"] = "Updating Release Failed!!!";
+
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    }
+ }
+  
+ /**
+  * Check for user's reserved location info and response with location ID and
+  * message if there is one. If not, response with message.
+  * 
+  * @param String $user_id Current log in ser ID
+  * @return JSON Object Message and information related to request.
+  * 
+ */
+// check for tag type
+ if ($task == 'getReserved') {
+     $user_id = $_POST['user_id'];
+    // get user's reserved location ID.
+    $result = mysqli_query($db, "SELECT * FROM users WHERE user_id = '$user_id'");
+
+    // check for empty result
+    if (mysqli_num_rows($result) > 0) {
+        // looping through all results
+        $response["user"] = array();
+ 
+        while ($row = mysqli_fetch_array($result)) {
+            // temp user array
+            $user = array();
+            $user["reserved_id"] = $row["reserved_id"];
+
+            // push single user array into final response array
+            array_push($response["user"], $user);
+        }
+    
+        if ($user["reserved_id"] != null){
+            // success
+            $response["success"] = 1;
+            $response["message"] = "You have a reserved spot: " . $user["reserved_id"];
+
+            // echoing JSON response
+            echo json_encode($response);
+        } 
+        else {
+            // no reservation found
+            $response["success"] = 0;
+            $response["message"] = "No Reserved Spot!!!";
+
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    }else {
+        // no reservation found
+        $response["success"] = 0;
+        $response["message"] = "No Reserved Spot!!!";
+
+       // echo no users JSON
+       echo json_encode($response);
+     }
+ }
    mysqli_close($db);
-//}
 ?>
--- min/ParkingLocPHP/login.php
+++ min/ParkingLocPHP/login.php
@@ -1,8 +1,18 @@
 <?php
 /**
- * Login service 
+ * -- Login service -- 
+ * 
+ * This file catch the HTTP Post entity sent from android with User Name
+ * and Password to check for user and matching password. If user exist, 
+ * the information of the login user is passing back as array in response 
+ * entity which is encoded in json format. 
  * 
  * @author Min
+ * 
+ * @param $name name of the login user
+ * @param $password password of the login user
+ * @return Json Encode The User information and related message if user is found
+ * in the database. If not just related message.
  */
 define("DB_HOST", "sfsuswe.com");
 define("DB_USER", "mphyo");
@@ -20,14 +30,13 @@ if (isset($_POST['user_name']) && isset($_POST['password'])) {
     // DB connection
     $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     
-    // get a user from users table
-    //$result = mysqli_query("SELECT * FROM users WHERE user_name = '". $_POST["user_name"]."' AND password = '". $_POST["password"]."'");
-    $result = mysqli_query($db, "SELECT user_id, user_name, password, loc_latitude, loc_longitude, loc_address
+    // query user info from database
+    $result = mysqli_query($db, "SELECT user_id, user_name, password, loc_latitude, loc_longitude, loc_address, reserved_id
         FROM users JOIN location_info WHERE users.home_loc_id = loc_id AND user_name = '$name' AND password = '$password'");
 
     // check for empty result
     if (mysqli_num_rows($result) > 0) {
-        // looping through all results
+        // if user found
         // user node
         $response["user"] = array();
  
@@ -41,7 +50,12 @@ if (isset($_POST['user_name']) && isset($_POST['password'])) {
             $location_info["home_loc_lat"] = $row["loc_latitude"];
             $location_info["home_loc_lng"] = $row["loc_longitude"];
             $location_info["home_loc_add"] = $row["loc_address"];
-            
+            if ($row["reserved_id"] == null){
+            $location_info["reserved_id"] = 0;
+            }
+            else {
+                 $location_info["reserved_id"] = $row["reserved_id"];
+            }
             // push single user into final response array
             array_push($response["user"], $location_info);
         }
